42d7688796800a658b6950e1ef64c5b965b58984
levaniakobidze <90929516+levaniakobidze@users.noreply.github.com>
2021-12-01 11:14:58 -0800

    add6

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ let menuButton = document.getElementById("menuButton");
 let leftBar = document.getElementById("leftBar");
 
     menuButton.addEventListener("click",function(){
-        leftBar.style.background = "blue"     
+        leftBar.style.display = "block"     
     });
 
     closeButton.addEventListener("click",function(){
-        leftBar.style.background = "red";
+        leftBar.style.display = "none";
     })
+
+    
+
+
+    
 
     
     
